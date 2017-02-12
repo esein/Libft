ccf6c6d17ee2a1b18466a824cac5c9fc3d0ff742
Gaetan CADIOU <gcadiou@e1r13p14.42.fr>
2017-02-12 17:17:17 +0100

    strcut now return char*

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gcadiou <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 05:25:10 by gcadiou           #+#    #+#             */
-/*   Updated: 2017/02/12 16:20:38 by gcadiou          ###   ########.fr       */
+/*   Updated: 2017/02/12 17:17:02 by gcadiou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,6 +151,6 @@ int					ft_strlentil(const char *s, char c, size_t i);
 
 void				*ft_realloc(void *ptr, size_t size, size_t size_actual);
 
-void				ft_strcut(char *s, int start, int end);
+char				*ft_strcut(char *s, int start, int end);
 
 #endif
