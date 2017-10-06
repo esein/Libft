f05090df999c68920c84035175db5e1202cc5c60
Gaetan CADIOU <gcadiou@e3r7p15.42.fr>
2017-10-06 21:31:11 +0200

    add putstr(malloc error) in memalloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: gcadiou <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/18 18:17:34 by gcadiou           #+#    #+#             */
-/*   Updated: 2016/12/01 05:45:56 by gcadiou          ###   ########.fr       */
+/*   Updated: 2017/10/06 21:30:39 by gcadiou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	*ft_memalloc(size_t size)
 	void	*mem;
 
 	if (!(mem = malloc(size)))
+	{
+		ft_putstr("malloc error");
 		return (NULL);
+	}
 	ft_bzero(mem, size);
 	return (mem);
 }
